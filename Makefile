6123f69971ee7f24c7bae4715d9aee04b3d278ff
nikmoon <consensuss@rambler.ru>
2017-02-28 14:46:31 +0700

    tuning build system for linux

--- Makefile
+++ Makefile
@@ -6,16 +6,27 @@
 .PHONY: all clean
 
 all: torrman
+#all: mainwindow.o
 
-torrman: torrman.o ctrlc_signal.o
-	g++ -o torrman torrman.o ctrlc_signal.o -pthread -ltorrent-rasterbar -lboost_system
+torrman: torrman.o main.o mainwindow.o mainwindow.h.o
+	g++ -o torrman torrman.o main.o mainwindow.o mainwindow.h.o \
+		-pthread -ltorrent-rasterbar -lboost_system `pkg-config --libs Qt5Widgets`
 	strip torrman
 
 torrman.o: torrman.cpp torrman.hpp
-	g++ -std=c++11 -c -o torrman.o torrman.cpp
+	g++ -std=c++11 -c -o torrman.o torrman.cpp `pkg-config --cflags Qt5Widgets`  -fPIC
 
-ctrlc_signal.o: ctrlc_signal.cpp ctrlc_signal.hpp
-	g++ -std=c++11 -c -o ctrlc_signal.o ctrlc_signal.cpp
+main.o: main.cpp mainwindow.h ui_mainwindow.h
+	g++ -std=c++11 -c -o main.o main.cpp `pkg-config --cflags Qt5Widgets` -fPIC
+
+mainwindow.o: mainwindow.cpp mainwindow.h ui_mainwindow.h
+	moc -omainwindow.h.moc.cpp mainwindow.h
+	g++ -std=c++11 -c -o mainwindow.h.o mainwindow.h.moc.cpp `pkg-config --cflags Qt5Widgets` -fPIC
+	g++ -std=c++11 -c -o mainwindow.o mainwindow.cpp `pkg-config --cflags Qt5Widgets` -fPIC
+	rm mainwindow.h.moc.cpp
+
+ui_mainwindow.h: mainwindow.ui
+	uic mainwindow.ui -o ui_mainwindow.h
 
 clean:
 	rm -rf torrman *.o
---
+++ ui_mainwindow.h
@@ -0,0 +1,105 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.6.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QHBoxLayout *horizontalLayout;
+    QGroupBox *groupBox;
+    QVBoxLayout *verticalLayout;
+    QPushButton *pushButton;
+    QTableWidget *tableWidget;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(646, 402);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        horizontalLayout = new QHBoxLayout(centralWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        groupBox = new QGroupBox(centralWidget);
+        groupBox->setObjectName(QStringLiteral("groupBox"));
+        verticalLayout = new QVBoxLayout(groupBox);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        pushButton = new QPushButton(groupBox);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        verticalLayout->addWidget(pushButton);
+
+
+        horizontalLayout->addWidget(groupBox);
+
+        tableWidget = new QTableWidget(centralWidget);
+        tableWidget->setObjectName(QStringLiteral("tableWidget"));
+
+        horizontalLayout->addWidget(tableWidget);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 646, 21));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        groupBox->setTitle(QApplication::translate("MainWindow", "GroupBox", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "PushButton", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
