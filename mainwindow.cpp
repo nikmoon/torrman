d0da45fc697ed9db49ee53586c6481169ea84f45
nikmoon <consensuss@rambler.ru>
2017-02-28 14:09:25 +0700

    It working!!!!

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,15 +10,16 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->tableWidget->setColumnCount(5);
-    //ui->tableWidget->setRowCount(10);
-    ses = new lt::session();
-    ses->set_alert_mask(lt::alert::all_categories);
     rowCount = 0;
+
+    ses = new lt::session();
+    ses->set_alert_mask(lt::alert::status_notification);
+    ses->post_torrent_updates();
 
     connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(openTorrentFile()));
 
     timer = new QTimer(this);
-    connect(timer, SIGNAL(timeout()), this, SLOT(updateView()));
+    connect(timer, SIGNAL(timeout()), this, SLOT(updateWidget()));
     timer->start(1000);
 }
 
@@ -30,20 +31,26 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::updateView()
+void MainWindow::updateWidget()
 {
-    //int rowCount = ui->tableWidget->rowCount();
-
     ses->pop_alerts(&alerts);
 
-    for(const lt::alert *alert: alerts)
+    for(lt::alert *alert: alerts)
     {
-        //rowCount++;
-        ui->tableWidget->insertRow(rowCount);
-        QString s = QString::fromStdString(alert->message());
-        QTableWidgetItem *item = new QTableWidgetItem(s);
-        ui->tableWidget->setItem(rowCount, 0, item);
-        rowCount++;
+        ses->post_torrent_updates();
+        switch (alert->type()) {
+        case lt::torrent_added_alert::alert_type:
+            addTorrent(static_cast<lt::torrent_added_alert*>(alert)->handle);
+            break;
+        case lt::state_update_alert::alert_type:
+            updateTorrentsStatus(static_cast<lt::state_update_alert*>(alert));
+            break;
+        case lt::state_changed_alert::alert_type:
+            updateTorrentState(static_cast<lt::state_changed_alert*>(alert));
+            break;
+        default:
+            break;
+        }
     }
 }
 
@@ -56,5 +63,85 @@ void MainWindow::openTorrentFile()
         atp.ti = new lt::torrent_info(fileName.toStdString());
         atp.save_path = ".";
         ses->async_add_torrent(atp);
+    }
+}
+
+
+void MainWindow::addTorrent(lt::torrent_handle h)
+{
+    ui->tableWidget->insertRow(rowCount);
+    torrentsRowMap[h.name()] = rowCount;
+    QString s = QString::fromStdString(h.name());
+    QTableWidgetItem *item = new QTableWidgetItem(s);
+    ui->tableWidget->setItem(rowCount, 0, item);
+    ui->tableWidget->setItem(rowCount, 1, new QTableWidgetItem());
+    ui->tableWidget->setItem(rowCount, 2, new QTableWidgetItem());
+    ui->tableWidget->setItem(rowCount, 3, new QTableWidgetItem());
+    ui->tableWidget->setItem(rowCount, 4, new QTableWidgetItem());
+    rowCount++;
+}
+
+
+void MainWindow::updateTorrentsStatus(lt::state_update_alert *alert)
+{
+    const double mb = 1024.0 * 1024.0;
+    double rate;
+    double progress;
+    QString s;
+    int rowNum;
+    QTableWidgetItem *itemState, *itemDlRate, *itemUpRate, *itemProgress;;
+    for(lt::torrent_status st: alert->status)
+    {
+        rowNum = torrentsRowMap[st.handle.name()];
+        itemState = ui->tableWidget->item(rowNum, 1);
+        itemDlRate = ui->tableWidget->item(rowNum, 2);
+        itemUpRate = ui->tableWidget->item(rowNum, 3);
+        itemProgress = ui->tableWidget->item(rowNum, 4);
+        if (st.download_payload_rate >= mb)
+        {
+            rate = st.download_payload_rate / mb;
+            s = QString::number(rate, 'f', 2) + QString(" MiB/s");
+        }
+        else
+        {
+            rate = st.download_payload_rate / 1024.0;
+            s = QString::number(rate, 'f', 2) + QString(" KiB/s");
+        }
+        itemDlRate->setText(s);
+
+        if (st.upload_payload_rate >= mb)
+        {
+            rate = st.upload_payload_rate / mb;
+            s = QString::number(rate, 'f', 2) + QString(" MiB/s");
+        }
+        else
+        {
+            rate = st.upload_payload_rate / 1024.0;
+            s = QString::number(rate, 'f', 2) + QString(" KiB/s");
+        }
+        itemUpRate->setText(s);
+        itemProgress->setText(QString::number(st.progress, 'f', 2));
+    }
+}
+
+
+void MainWindow::updateTorrentState(lt::state_changed_alert *alert)
+{
+    int rowNum = torrentsRowMap[alert->handle.name()];
+    QTableWidgetItem *item = ui->tableWidget->item(rowNum, 1);
+
+    switch (alert->state) {
+    case lt::torrent_status::downloading:
+        item->setText(QString("downloading"));
+        break;
+    case lt::torrent_status::seeding:
+        item->setText(QString("seeding"));
+        break;
+    case lt::torrent_status::checking_files:
+        item->setText(QString("cheking"));
+        break;
+    default:
+        item->setText(QString("unknown"));
+        break;
     }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -9,6 +9,7 @@
 #define BOOST_ASIO_SEPARATE_COMPILATION
 #include <libtorrent/session.hpp>
 #include <libtorrent/alert_types.hpp>
+#include <libtorrent/torrent_handle.hpp>
 
 #include <QMainWindow>
 #include <QTimer>
@@ -31,17 +32,22 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+
+    void addTorrent(lt::torrent_handle h);
+    void updateTorrentsStatus(lt::state_update_alert *a);
+    void updateTorrentState(lt::state_changed_alert *a);
+
 private:
     Ui::MainWindow *ui;
 
     QTimer *timer;
     lt::session *ses;
-    std::map<std::string, int> torrentRow;
+    std::map<std::string, int> torrentsRowMap;
     std::deque<lt::alert *> alerts;
     int rowCount;
 
 private slots:
-    void updateView();
+    void updateWidget();
     void openTorrentFile();
 };
 
